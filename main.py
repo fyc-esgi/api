6fd98d4d856e67c23137b589d65a311be2caf2e4
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2020-12-09 19:33:59 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, Response, jsonify
+
+from pokemon_controller import PokemonController
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def default():
+    return jsonify({'test': 'Hello world'})
+
+
+@app.route('/pokemon', methods=['POST'])
+def add_pokemon():
+    action = PokemonController.addPokemon(request.get_json())
+    if action == 1:
+        return jsonify({'message': 'Successfully created'}), 201
+    elif action == -1:
+        return jsonify({'message': "Wrong attributes, needs : 'name', 'element_type', 'hp', 'attack', 'defense' and "
+                                   "'speed'."}), 400
+    elif action == -2:
+        return jsonify({'message': 'Already exists'}), 400
+    else:
+        return jsonify({'message': 'Bad request'}), 400
+
+
+@app.route('/pokemon', methods=['PUT'])
+def update_pokemon():
+    action = PokemonController.updatePokemon(request.get_json())
+    if action == 1:
+        return jsonify({'message': 'Successfully updated'}), 200
+    elif action == -1:
+        return jsonify({'message': "Wrong attributes, needs : 'name', 'element_type', 'hp', 'attack', 'defense' and "
+                                   "'speed'."}), 400
+    elif action == -2:
+        return jsonify({'message': "Doesn't exist."}), 404
+    else:
+        return jsonify({'message': 'Bad request'}), 400
+
+
+@app.route('/pokemon', methods=['DELETE'])
+def delete_pokemon():
+    action = PokemonController.deletePokemon(request.get_json()['name'])
+    if action == 1:
+        return jsonify({'message': 'Successfully deleted'}), 200
+    elif action == -1:
+        return jsonify({'message': "Doesn't exist."}), 400
+    else:
+        return jsonify({'message': 'Bad request'}), 400
+
+
+@app.route('/pokemon', methods=['GET'])
+def get_all_pokemon():
+    return jsonify({'data': PokemonController.getAll()}), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ pokemon_controller.py
@@ -0,0 +1,50 @@
+class PokemonController:
+    """Pokemon class controller """
+    pokemons = []
+    pok_attributes = ['name', 'element_type', 'hp', 'attack', 'defense', 'speed']
+
+    @staticmethod
+    def addPokemon(json: dict) -> int:
+        if not PokemonController.is_pokemon(json):
+            return -1
+        if PokemonController.get_index(json['name']) != -1:
+            return -2
+        PokemonController.pokemons.append(json)
+        return 1
+
+    @staticmethod
+    def updatePokemon(json: dict) -> int:
+        if not PokemonController.is_pokemon(json):
+            return -1
+        index = PokemonController.get_index(json['name'])
+        if index == -1:
+            return -2
+        PokemonController.pokemons[index] = json
+        return 1
+
+    @staticmethod
+    def deletePokemon(pokemon_name: str) -> int:
+        index = PokemonController.get_index(pokemon_name)
+
+        if index == -1:
+            return -1
+        del PokemonController.pokemons[index]
+        return 1
+
+    @staticmethod
+    def getAll() -> list:
+        return PokemonController.pokemons
+
+    @staticmethod
+    def get_index(pokemon_name) -> int:
+        for index, pokemon in enumerate(PokemonController.pokemons):
+            if pokemon['name'] == pokemon_name:
+                return index
+        return -1
+
+    @staticmethod
+    def is_pokemon(json: dict) -> bool:
+        for pok_att in PokemonController.pok_attributes:
+            if pok_att not in json:
+                return False
+        return True
