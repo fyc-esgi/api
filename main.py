45e3b7110a2b4cea21da632c3c5af4c4f54130d2
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2020-12-10 12:24:23 +0100

    add cors

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from route.pokemon_router import pokemon_api
 from route.trainer_router import trainer_api
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(pokemon_api)
 app.register_blueprint(trainer_api)
@@ -15,4 +17,4 @@ def default():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
